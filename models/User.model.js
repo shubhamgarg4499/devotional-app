ac8ad3afbb475a06bd3fad43492490cd5a94cb36
Shubham garg <shubhamgarg10904@gmail.com>
2024-12-09 14:13:22 +0530

    update

--- models/User.model.js
+++ models/User.model.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         unique: true,
+        sparse: true,
         type: String,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
         lowercase: true,
